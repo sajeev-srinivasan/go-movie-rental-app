093d530b8daf7583426864c9818344329ca985e8
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2024-01-31 16:14:33 +0530

    [Sajeev|Rajdeep] Refactor. hello world handler

--- internal/app/handlers/hello_world.go
+++ internal/app/handlers/hello_world.go
@@ -1,1 +1,10 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetHelloWorld(context *gin.Context) {
+	context.JSON(http.StatusOK, "Hello")
+}
--- internal/app/handlers/hello_world_test.go
+++ internal/app/handlers/hello_world_test.go
@@ -1,1 +1,30 @@
 package handlers
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHelloWorldShouldReturn200(t *testing.T) {
+	engine := gin.Default()
+
+	engine.GET("/helloWorld", GetHelloWorld)
+
+	request, err := http.NewRequest(http.MethodGet, "/helloWorld", nil)
+	require.NoError(t, err)
+
+	responseRecorder := httptest.NewRecorder()
+	engine.ServeHTTP(responseRecorder, request)
+
+	var responseBody string
+	err = json.NewDecoder(responseRecorder.Body).Decode(&responseBody)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Equal(t, "Hello, World!", responseBody)
+}
