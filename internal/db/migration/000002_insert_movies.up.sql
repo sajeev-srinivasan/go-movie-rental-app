da9ef860e5a78e7b36c33383a1641f93611cef1d
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2024-02-05 12:15:15 +0530

    [Sajeev|Rajdeep] Add. sql script to insert data

---
+++ internal/db/migration/000002_insert_movies.up.sql
@@ -0,0 +1,5 @@
+insert into movies (id, title, year, genre, actors) VALUES ('tt0372784', 'Batman Begins', 2005, 'Crime', 'Christian Bale'),
+                                                           ('tt2098632', 'Batman: The Dark Knight', 2008, 'Action', 'Daniel Gidlow'),
+                                                           ('tt15398776', 'Oppenheimer', 2023, 'Drama', 'Cillian Murphy'),
+                                                           ('tt1130884', 'Shutter Island', 2010, 'Mystery', 'Leonardo DiCaprio'),
+                                                           ('tt1375666', 'Inception', 2010, 'Adventure', 'Leonardo DiCaprio')
